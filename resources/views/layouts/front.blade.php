81715227427baf2c1f22a7c83a280fe7f7e792d8
toto <test@test.com>
2024-01-08 20:54:38 +0200

    fa link

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <title>Laravel Quickstart - Basic</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     </head>
     <body>
